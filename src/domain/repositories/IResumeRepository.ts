4c967b882e5076c205f96ac867da593cf6d3469f
Shamsiddin <xamroqulovsh7@gmail.com>
2026-07-10 19:02:08 +0500

    feat: add resume to infratructure/repositories

--- src/domain/repositories/IResumeRepository.ts
+++ src/domain/repositories/IResumeRepository.ts
@@ -1,8 +1,8 @@
 import { Resume } from "../entities/Resume";
 
 export interface IResumeRepository {
-  findById(resumeId: string): Promise<Resume | void>
-  findByUserId(userId: string): Promise<Resume>
+  findById(resumeId: string): Promise<Resume | null>
+  findByUserId(userId: string): Promise<Resume | null>
   findAll(): Promise<Resume[]>
   save(data: Resume): Promise<Resume>
   remove(resumeId: string): Promise<void>
---
+++ src/infrastructure/db/repositories/PrismaResumeRepository.ts
@@ -0,0 +1,69 @@
+import { Injectable } from "@nestjs/common";
+import { IResumeRepository } from "src/domain/repositories/IResumeRepository";
+import { PrismaService } from "../prisma.service";
+import { Resume } from "src/domain/entities/Resume";
+
+
+@Injectable()
+export class PrismaResumeRepository implements IResumeRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async findById(resumeId: string): Promise<Resume | null> {
+    const resume = await this.prismaService.resume.findUnique({where: {id: resumeId}});
+    if(!resume) return null;
+    return this.toDomain(resume);
+  };
+
+  async findByUserId(userId: string): Promise<Resume | null> {
+    const user = await this.prismaService.resume.findFirst({where: {userId}});
+    if(!user) return null
+    return this.toDomain(user);
+  };
+
+  async findAll(): Promise<Resume[]> {
+    const resumes = await this.prismaService.resume.findMany();
+    return resumes.map((resume) => this.toDomain(resume));
+  };
+
+  async save(data: Resume): Promise<Resume> {
+    const row = await this.prismaService.resume.upsert({
+      where: {id: data.id ?? ''},
+      update: this.toPersistance(data),
+      create: this.toPersistance(data)
+    });
+    return this.toDomain(row);
+  };
+
+  async remove(resumeId: string): Promise<void> {
+    await this.findById(resumeId);
+    await this.prismaService.resume.delete({where: {id: resumeId}});
+  };
+
+  private toDomain(resume: any): Resume {
+    return new Resume({
+      id: resume.id,
+      userId: resume.userId,
+      title: resume.title,
+      summary: resume.summary,
+      city: resume.city,
+      salaryRange: resume.salaryRange,
+      fileUrl: resume.fileUrl,
+      status: resume.status,
+      createdAt: resume.createdAt
+    });
+  };
+
+  private toPersistance(resume: Resume): any {
+    return {
+      id: resume.id,
+      userId: resume.userId,
+      ttile: resume.title,
+      summary: resume.summary,
+      city: resume.city,
+      salaryRange: resume.salaryRange,
+      fileUrl: resume.fileUrl,
+      status: resume.status,
+      createdAt: resume.createdAt
+    };
+  };
+}
