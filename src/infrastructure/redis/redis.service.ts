614803538f8a75d38156bcc3ef20584742de7b56
Shamsiddin <xamroqulovsh7@gmail.com>
2026-07-18 05:09:41 +0500

    feat: add redis service

---
+++ src/infrastructure/redis/redis.service.ts
@@ -0,0 +1,29 @@
+import { InjectRedis } from "@nestjs-modules/ioredis";
+import Redis from "ioredis";
+import { ICacheRedisInterface } from "src/domain/services/ICacheRedisService";
+
+export class CacheRedisService implements ICacheRedisInterface {
+  constructor(@InjectRedis() private readonly redis: Redis) {}
+
+  async get<T>(key: string): Promise<T | null> {
+    const data = await this.redis.get(key);
+    if(!data) return null;
+    return JSON.parse(data) as T;
+  };
+
+  async set<T>(key: string, value: T, ttlSeconds: 120): Promise<void> {
+    const data = JSON.stringify(value);
+    await this.redis.set(key, data, 'EX', ttlSeconds, 'NX');
+  };
+
+  async delete(key: string): Promise<void> {
+    await this.redis.del(key);
+  };
+
+  async deleteByPattern(pattern: string): Promise<void> {
+    const keys = await this.redis.keys(pattern);
+    if(keys.length > 0) {
+      await this.redis.del(...keys);
+    };
+  };
+};
